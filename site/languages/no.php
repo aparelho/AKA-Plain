6cd3ff834fdb937576d8ae9a381f82489de3136e
aparelho <liga@aparelho.tv>
2020-10-04 22:46:37 -0300

    {panel} archive and news blueprints

---
+++ site/languages/no.php
@@ -0,0 +1,15 @@
+<?php
+
+return [
+    'code' => 'no',
+    'default' => false,
+    'direction' => 'ltr',
+    'locale' => [
+        'LC_ALL' => 'no_NO'
+    ],
+    'name' => 'norwegian',
+    'translations' => [
+
+    ],
+    'url' => NULL
+];
